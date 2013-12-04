c5ffa32157015d60229a35dbeeed840f91f07f63
Pranav Vashi <neobuddy89@gmail.com>
2013-12-04 09:40:41 -0800

    post-init: Perform cleanup
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- sbin/ext/post-init.sh
+++ sbin/ext/post-init.sh
@@ -4,6 +4,9 @@ BB=/system/xbin/busybox
 
 $BB mount -t rootfs -o remount,rw rootfs;
 $BB mount -o remount,rw /system;
+$BB mount -o remount,rw,nosuid,nodev /cache;
+$BB mount -o remount,rw,nosuid,nodev /data;
+$BB mount -o remount,rw /;
 
 # set sysrq to 2 = enable control of console logging level
 echo "2" > /proc/sys/kernel/sysrq;
@@ -14,3 +17,13 @@ echo "0" > /proc/sys/kernel/kptr_restrict;
 (
 	$BB sh /sbin/ext/run-init-scripts.sh;
 )&
+
+# cleaning
+$BB rm -rf /cache/lost+found/* 2> /dev/null;
+$BB rm -rf /data/lost+found/* 2> /dev/null;
+$BB rm -rf /data/tombstones/* 2> /dev/null;
+$BB rm -rf /data/anr/* 2> /dev/null;
+
+# mark boot completion
+$BB touch /data/.bootcheck;
+$BB echo "$(date)" > /data/.bootcheck;
